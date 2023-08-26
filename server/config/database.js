0f17c65545c02624dd820086bd3c0defac96f783
Lza-etc <emj688531@gmail.com>
2023-08-27 00:26:54 +0530

    added mongoose strict

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv')
 dotenv.config();
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', true);
 const connect = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
